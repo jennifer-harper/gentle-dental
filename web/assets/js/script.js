a11ed909533f17c9407359ef29d2f4ddc77bc29b
jennifer <jennifer@ambitious.co.nz>
2023-12-20 15:17:59 +1300

    nav progress

--- web/assets/js/script.js
+++ web/assets/js/script.js
@@ -22,15 +22,104 @@ $(document).ready(function () {
   })
 })
 
-/////navigation
-$('.toggle').click(function () {
-  'use strict'
-  $('nav ul').slideToggle()
-})
+// $(document).ready(function () {
+//   'use strict'
 
-$(window).resize(function () {
+//   // Hide dropdown initially
+//   $('.dropdown-nav').hide()
+
+//   // Toggle navigation on click
+//   $('.toggle').click(function () {
+//     $('nav ul').slideToggle() // Change the selector to target the top-level ul
+//     $('.dropdown-nav').toggleClass('open') // Add or remove the 'open' class
+//   })
+
+//   // Handle dropdown hover
+//   $('.dropdown').hover(function () {
+//     // Mouse enters dropdown
+//     $('.dropdown-nav').stop().slideDown().addClass('open')
+//   })
+
+//   // Close dropdown on anchor click
+//   $('.dropdown-nav a').click(function (e) {
+//     e.stopPropagation() // Prevent the dropdown from closing when clicking an anchor
+//   })
+
+//   // Close dropdown when clicking outside the dropdown or when mouse leaves .dropdown-nav
+//   $(document).on('click', function (e) {
+//     if (!$(e.target).closest('.dropdown').length) {
+//       $('.dropdown-nav').slideUp().removeClass('open') // Change the selector here
+//     }
+//   })
+
+//   // Prevent closing when mouse is over .dropdown-nav
+//   $('.dropdown-nav').hover(
+//     function () {
+//       // Mouse enters dropdown-nav
+//     },
+//     function () {
+//       // Mouse leaves dropdown-nav
+//       $('.dropdown-nav').slideUp().removeClass('open') // Change the selector here
+//     }
+//   )
+// })
+
+$(document).ready(function () {
   'use strict'
-  if ($(window).width() > 992) {
+  $('.dropdown-nav').hide()
+
+  $('.toggle').click(function () {
+    $('nav ul').slideToggle()
+  })
+
+  const mediaQuery = window.matchMedia('(max-width: 992px)')
+
+  if (mediaQuery.matches) {
     $('nav ul').removeAttr('style')
+
+    // Toggle navigation on click
+    $('.dropdown').click(function () {
+      $('.dropdown-nav').toggleClass('open') // Add or remove the 'open' class
+    })
+
+    // Handle dropdown hover
+    $('.dropdown').hover(function () {
+      // Mouse enters dropdown
+      $('.dropdown-nav').stop().slideDown().addClass('open')
+    })
+  } else {
+    // Toggle navigation on click
+    $('.dropdown').click(function () {
+      $('.dropdown-nav').toggleClass('open') // Add or remove the 'open' class
+    })
+
+    // Handle dropdown hover
+    $('.dropdown').hover(function () {
+      // Mouse enters dropdown
+      $('.dropdown-nav').stop().slideDown().addClass('open')
+    })
+
+    // Close dropdown on anchor click
+    $('.dropdown-nav a').click(function (e) {
+      e.stopPropagation() // Prevent the dropdown from closing when clicking an anchor
+    })
+
+    // Close dropdown when clicking outside the dropdown or when mouse leaves .dropdown-nav
+    $(document).on('click', function (e) {
+      if (!$(e.target).closest('.dropdown').length) {
+        $('.dropdown-nav').slideUp().removeClass('open') // Change the selector here
+      }
+    })
+
+    // Prevent closing when mouse is over .dropdown-nav
+    $('.dropdown-nav').hover(
+      function () {
+        // Mouse enters dropdown-nav
+      },
+      function () {
+        // Mouse leaves dropdown-nav
+        $('.dropdown-nav').slideUp().removeClass('open') // Change the selector here
+      }
+    )
   }
 })
