3632bbbc936e8b1ebb2475ef8f3dfe29e0bdc7ff
jennifer <jennifer@ambitious.co.nz>
2024-01-08 16:30:22 +1300

    progress with homepage

--- web/assets/js/slider.js
+++ web/assets/js/slider.js
@@ -17,3 +17,24 @@ var swiper = new Swiper('.slider-one', {
     prevEl: '.swiper-button-prev',
   },
 })
+
+var swiper = new Swiper('.slider-testimonial', {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  centeredSlides: true,
+  loop: true,
+  // autoplay: {
+  //   delay: 8000,
+  //   disableOnInteraction: false,
+  // },
+
+  breakpoints: {
+    1024: {
+      slidesPerView: 2,
+    },
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+})
